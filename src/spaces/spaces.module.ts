03c3afde2daf81a89bdab9e6615b542e5a44d9bd
Felipe Couto <116991833+felipecouto0101@users.noreply.github.com>
2025-04-29 16:56:18 -0300

    Revert "Feature/database"

--- src/spaces/spaces.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpacesService } from './spaces.service';
-import { SpacesController } from './spaces.controller';
-
-@Module({
-  providers: [SpacesService],
-  controllers: [SpacesController]
-})
-export class SpacesModule {}
