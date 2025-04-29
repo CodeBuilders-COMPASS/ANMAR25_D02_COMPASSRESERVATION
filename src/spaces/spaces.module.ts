efecaa1d1823fff21098ea64d7248a6fbfad682c
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-04-29 18:01:45 -0300

    feat(spaces); implement prisma module in spaces

--- src/spaces/spaces.module.ts
+++ src/spaces/spaces.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SpacesService } from './spaces.service';
 import { SpacesController } from './spaces.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
 
 @Module({
+  imports: [PrismaModule],
   providers: [SpacesService],
   controllers: [SpacesController]
 })
 export class SpacesModule {}
+
+
