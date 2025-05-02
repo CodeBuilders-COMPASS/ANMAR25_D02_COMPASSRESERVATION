ad9b6394b52d9196371dc83f1f65691ac8a1e970
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-05-02 10:35:47 -0300

    feat(auth): create auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,15 @@
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
+export class AuthController {
+  constructor() {
+    // Constructor logic if needed
+  }
+  async createToken(){
+    //return this.jwtService.signAsync({ id: user.id });
+    // Logic to create a token
+  }
+  async checkToken(){
+    // return this.jwtService.verifyAsync(token);
+  }
+}
