e4027375a2fd19d71181c01585a98f6809379210
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-05-04 20:59:11 -0300

    feat(spaces): controller tests add

--- src/spaces/tests/spaces.controller.spec.ts
+++ src/spaces/tests/spaces.controller.spec.ts
@@ -1,16 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { SpacesController } from '../spaces.controller';
 import { SpacesService } from '../spaces.service';
 
 describe('SpacesController', () => {
   let controller: SpacesController;
+  let service: SpacesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [SpacesController],
+      providers: [
+        {
+          provide: SpacesService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<SpacesController>(SpacesController);
+    controller = moduleRef.get<SpacesController>(SpacesController);
+    service = moduleRef.get<SpacesService>(SpacesService);
   });
 
   it('should be defined', () => {
