39ec68ece9eebac170f66d425049c7cc9df313b7
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2025-05-04 21:12:52 -0300

    feat(spaces): add ResourcesValidationExistPipe

--- src/spaces/spaces.controller.ts
+++ src/spaces/spaces.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Post, Body, Get, Param, Query, ParseIntPipe, Delete, Patch } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query, ParseIntPipe, Delete, Patch, UsePipes } from '@nestjs/common';
 import { FilterSpaceDto } from './dto/filter-space.dto';
 import { CreateSpaceDto } from './dto/create-space.dto';
 import { UpdateSpaceDto } from './dto/update-space.dto';
 import { SpacesService } from './spaces.service';
 import { PositiveIntPipe } from 'src/pipes/positive-int.pipe';
 import { SpaceExistsPipe } from 'src/pipes/space-exists.pipe';
+import { ResourcesValidationExistPipe } from 'src/pipes/validate-resources-exist.pipe';
 
 @Controller('spaces')
 export class SpacesController {
     constructor(private readonly spacesService: SpacesService) {}
 
     @Post()
+    @UsePipes( ResourcesValidationExistPipe )
     async create(@Body() createSpaceDto: CreateSpaceDto) {
         return this.spacesService.create(createSpaceDto);
     }
