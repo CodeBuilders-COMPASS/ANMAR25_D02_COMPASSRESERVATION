11a7fcb5ab30b0dd095bec00396daf81ca736cbf
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2025-05-05 18:22:43 -0300

    feat(reservation): implements CPF filter

--- src/reservations/dto/filter-reservation.dto.ts
+++ src/reservations/dto/filter-reservation.dto.ts
@@ -1,8 +1,16 @@
-import { IsInt, IsOptional, IsEnum, IsDate } from 'class-validator';
+import { IsInt, IsOptional, IsEnum, IsDate, IsString, Matches } from 'class-validator';
 import { ReservationStatus } from '@prisma/client';
 import { Type } from 'class-transformer';
 
 export class FilterReservationDto {
+
+  @IsOptional()
+  @IsString()
+    @Matches(
+      /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+      { message: 'CPF must be in the format XXX.XXX.XXX-XX' },
+    )
+    cpf?: string;
 
   @IsOptional()
   @IsEnum(ReservationStatus)
