1ea7b68083ab7c22e3fd78a82ad914a910057ed5
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-05-05 14:03:55 -0300

    tests(resources): add resources.controller.spec.ts spec

---
+++ src/resources/tests/resources.controller.spec.ts
@@ -0,0 +1,39 @@
+import { Test } from '@nestjs/testing';
+import { ResourceController } from '../resources.controller';
+import { ResourceService } from '../resources.service';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ResourceExistsPipe } from '../../pipes/resource-exists.pipe';
+import { prismaMock } from '../../__mocks__/prisma.mock';
+import { CreateResourceDto } from '../dto/create-resource.dto';
+import { UpdateResourceDto } from '../dto/update-resource.dto';
+
+describe('SpacesController', () => {
+  let controller: ResourceController;
+  let service: ResourceService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [ResourceController],
+      providers: [
+        ResourceService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+        {
+          provide: ResourceExistsPipe,
+          useFactory: (prisma: PrismaService) => new ResourceExistsPipe(prisma),
+          inject: [PrismaService],
+        },
+      ],
+    }).compile();
+
+    controller = moduleRef.get<ResourceController>(ResourceController);
+    service = moduleRef.get<ResourceService>(ResourceService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+});
