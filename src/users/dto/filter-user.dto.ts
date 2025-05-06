ca76c8de1094e311f78490d154f40a50086efc19
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2025-05-06 14:42:15 -0300

    feat(users): add filterDto

---
+++ src/users/dto/filter-user.dto.ts
@@ -0,0 +1,33 @@
+import { Type } from "class-transformer";
+import {
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+} from "class-validator";
+import { StatusEnum } from "src/enums/status.enum";
+
+export class FilterUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(StatusEnum)
+  status?: StatusEnum
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  page?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  limit?: number;
+}
