705a9c4691b1cb1b67f4435f90c50d2e6a4e2771
Janaína Ferreira Monteiro <janainaferreiramonteiro@hotmail.com>
2025-05-06 17:08:51 -0300

    feat(app.service): add welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome API Reservation http://localhost:3000/api/v1/auth/login';
   }
 }
